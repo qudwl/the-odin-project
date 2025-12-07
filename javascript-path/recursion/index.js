de3bc8b9809ec5fcd522c5e047e0861f178e7dca
Byeongji Lim <byeongjilim@gmail.com>
2025-12-07 20:41:05 +0100

    feat: add mergeSort

--- javascript-path/recursion/index.js
+++ javascript-path/recursion/index.js
@@ -1,3 +1,9 @@
+/**
+ * fibonnaci numbers with loops
+ * 
+ * @param {number} num 
+ * @returns an array of fibonnaci numbers
+ */
 function fibs(num) {
     const arr = []
 
@@ -12,6 +18,12 @@ function fibs(num) {
     return arr
 }
 
+/**
+ * fibonnaci numbers with recursion
+ * 
+ * @param {number} num 
+ * @returns an array of fibonnaci numbers
+ */
 function fibsRec(num) {
     if (num === 0) { 
         return [] 
@@ -31,9 +43,53 @@ function fibsRec(num) {
     }
 }
 
+/**
+ * Merge sort with recursion
+ * 
+ * @param {Array} arr 
+ * @returns sorted array
+ */
+function mergeSort(arr) {
+    if (arr.length === 0) return []
+    if (arr.length === 1) return arr
+    const split = Math.floor(arr.length / 2)
+    let arr1 = arr.slice(0, split)
+    let arr2 = arr.slice(split)
+    const resultArr = []
+
+    arr1 = mergeSort(arr1)
+    arr2 = mergeSort(arr2)
+
+    let i1 = 0, i2 = 0
+
+    while(arr1.length > i1 && arr2.length > i2) {
+        if (arr1[i1] <= arr2[i2]) {
+            resultArr.push(arr1[i1])
+            i1++
+        } else {
+            resultArr.push(arr2[i2])
+            i2++
+        }
+    }
+
+    if (arr1.length > i1) {
+        resultArr.push(...arr1.slice(i1))
+    } else if (arr2.length > i2) {
+        resultArr.push(...arr2.slice(i2))
+    }
+
+    return resultArr
+}
+
 function print(msg) {
     console.log(msg)
 }
 
-print(fibs(8))
-print(fibsRec(8))
+print(`Fibonnaci with loops: [${fibs(8)}]`)
+print(`Fibonnaci with recursion: [${fibsRec(8)}]`)
+
+print(`Merge Sort []: [${mergeSort([])}]`)
+print(`Merge Sort [73]: [${mergeSort([73])}]`)
+print(`Merge Sort [1, 2, 3, 4, 5]: [${mergeSort([1, 2, 3, 4, 5])}]`)
+print(`Merge Sort [3, 2, 1, 13, 8, 5, 0, 1]: [${mergeSort([3, 2, 1, 13, 8, 5, 0, 1])}]`)
+print(`Merge Sort [105, 79, 100, 110]: [${mergeSort([105, 79, 100, 110])}]`)
